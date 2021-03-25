07c2b543a76877bab6b2cfd27545ef14ea6863c8
Harry Bett <harrybett89@gmail.com>
2021-03-25 15:09:02 +0300

    add input property binding

--- src/app/goal-detail/goal-detail.component.ts
+++ src/app/goal-detail/goal-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Goals } from '../goals';
 
 @Component({
   selector: 'app-goal-detail',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoalDetailComponent implements OnInit {
 
+  @Input() goal: Goals | undefined;
   constructor() { }
 
   ngOnInit(): void {
