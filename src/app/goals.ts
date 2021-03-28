2893dac8e003638f590b58212c59a30dce8b6620
Harry Bett <harrybett89@gmail.com>
2021-03-28 04:40:08 +0300

    change description attribute

--- src/app/goals.ts
+++ src/app/goals.ts
@@ -1,6 +1,6 @@
 export class Goals {
   showDescription:boolean;
-  constructor(public id: number, public name: string, public description: string){
+  constructor(public id: number, public name: string, public description: any){
     this.showDescription=false;
   }  
 }
