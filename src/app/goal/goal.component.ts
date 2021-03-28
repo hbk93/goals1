7e86fb8599d4ec8edef5708d546d5bde643f2741
Harry Bett <harrybett89@gmail.com>
2021-03-28 15:38:37 +0300

    define logic for deleteGoal()

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -22,9 +22,13 @@ export class GoalComponent implements OnInit {
     this.goals[index].showDescription = !this.goals[index].showDescription;
   }
 
-  completeGoal(isComplete:any, index:any){
+  deleteGoal(isComplete:any, index:any){
     if (isComplete) {
-      this.goals.splice(index,1);
+      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+
+      if (toDelete){
+        this.goals.splice(index,1)
+      }
     }
   }
 
