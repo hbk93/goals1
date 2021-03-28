1baad6a103d1e2a06ab99ba6af189d72781c750e
Harry Bett <harrybett89@gmail.com>
2021-03-28 12:12:43 +0300

    edit document

--- src/app/goals.ts
+++ src/app/goals.ts
@@ -1,6 +1,6 @@
 export class Goals {
   showDescription:boolean;
-  constructor(public id: number, public name: string, public description: any, public completDate: Date){
+  constructor(public id: number, public name: string, public description: any, public completeDate: Date){
     this.showDescription=false;
   }  
 }
