488d50841a1d2131cfeb4820a0024615043a0973
Harry Bett <harrybett89@gmail.com>
2021-03-28 10:47:41 +0300

    add completeDate property

--- src/app/goals.ts
+++ src/app/goals.ts
@@ -1,6 +1,6 @@
 export class Goals {
   showDescription:boolean;
-  constructor(public id: number, public name: string, public description: any){
+  constructor(public id: number, public name: string, public description: any, public completDate: Date){
     this.showDescription=false;
   }  
 }
